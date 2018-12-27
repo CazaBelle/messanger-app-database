4e8d72b1ecde466bcc27cbaefe0e27e3dc7bda26
Carol Gardiner <carolan.gardiner@gmail.com>
2018-12-27 14:02:03 +0000

    Added edit functionality

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ require 'pry'
 require_relative 'datamapper_setup'
 
 class Talk2me < Sinatra::Base
+  enable :sessions
+  enable :method_override 
 
 
   get '/' do
@@ -14,17 +16,28 @@ class Talk2me < Sinatra::Base
     erb :index
   end
 
+  get '/message/:id' do |id|
+    @message = Message.get!(id)
+    erb(:full_message)
+  end
+
+  get '/message/:id/edit' do
+    @message = Message.get(params[:id])
+    erb(:edit_message)
+  end
+  
+  put '/message/:id' do
+    @message = Message.get(params[:id])
+    @message.update!(:message => params[:message])
+    redirect '/'
+  end 
+
   post '/message' do
     Message.create(:message => params[:message])
     redirect '/'
   end
 
-  get '/full_message/:id' do |id|
-    @message = Message.get!(id)
-    erb(:full_message)
-  end
-  
-
   run! if app_file == $0
 
 end
+
--- datamapper_setup.rb
+++ datamapper_setup.rb
@@ -3,5 +3,5 @@ require './lib/message'
 
 DataMapper.setup(:default, "postgres://localhost/messenger_db_#{ENV["RACK_ENV"]}")
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.auto_migrate!
+DataMapper.auto_upgrade!
 DataMapper.finalize

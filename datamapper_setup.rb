5c0349c715008fd36f3c54fcb605899f5cf5b7e1
Carol Gardiner <carolan.gardiner@gmail.com>
2018-12-28 16:23:33 +0000

    Removed migrate and upgrade from my datamapper_setup.rb so that it is only done in the command line

--- datamapper_setup.rb
+++ datamapper_setup.rb
@@ -3,5 +3,4 @@ require './lib/message'
 
 DataMapper.setup(:default, "postgres://localhost/messenger_db_#{ENV["RACK_ENV"]}")
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.auto_upgrade!
 DataMapper.finalize
